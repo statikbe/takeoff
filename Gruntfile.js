7f1b3f64664084daecd4b1db736cc30eefaba788
Bart Verbruggen <bart@statik.be>
2014-11-05 20:20:52 +0100

    add grunt time + navbar changes and form

--- Gruntfile.js
+++ Gruntfile.js
@@ -245,6 +245,7 @@ module.exports = function(grunt) {
 
     // Where we tell Grunt we plan to use this plug-in.
     require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
 
     // Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('default', ['build', 'watch']);
