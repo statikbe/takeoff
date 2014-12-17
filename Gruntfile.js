127f6875827a04b4d59d38808389746a84328e07
Steven <steven@statik.be>
2014-12-17 17:37:15 +0100

    Update svg2png

--- Gruntfile.js
+++ Gruntfile.js
@@ -100,9 +100,10 @@ module.exports = function(grunt) {
             all: {
                 // specify files in array format with multiple src-dest mapping
                 files: [
-                    // rasterize all SVG files in "img" and its subdirectories to "img/png"
+                    // rasterize all SVG files in "img/svg" and its subdirectories to "img/svg/fallback"
                     {
-                        src: ['img/**/*.svg'],
+                        cwd: 'img/svg/',
+                        src: ['**/*.svg'],
                         dest: 'img/svg/fallback/'
                     }
                 ]
