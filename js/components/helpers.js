ac73370761f074fd6f620c9daa06464f835eb35b
Niels <niels@statik.be>
2016-07-07 15:50:59 +0200

    Clean up flyout js, move some things to helpers

--- js/components/helpers.js
+++ js/components/helpers.js
@@ -1,19 +1,49 @@
 window.app = window.app || {};
 
-app.helpers = (function() {
-    var localStorageAvailable = function() {
-        var testKey = 'localstorageTest';
-        try {
-            localStorage.setItem(testKey, testKey);
-            localStorage.removeItem(testKey);
-            return true;
-        } catch (e) {
-            return false;
+app.helpers = (function () {
+
+    var $body = $('body');
+    
+    var _debounce = function (func, wait, immediate) {
+
+        var timeout;
+
+        return function () {
+
+            var context = this, args = arguments;
+            var later = function () {
+                timeout = null;
+                if (!immediate) {
+                    func.apply(context, args);
+                }
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) {
+                func.apply(context, args);
+            }
+        };
+    };
+
+    var _getContentProperty = function (element, pseudoElement) {
+
+        if (!!getComputedStyle) {
+            //  getComputedStyle is not supported
+            return '';
         }
+
+        return getComputedStyle(element, pseudoElement).getPropertyValue('content');
+    };
+
+    var _isBreakpointActive = function (breakpointKey) {
+        return this.getContentProperty($body[0], ':after').indexOf(breakpointKey) < 0;
     };
 
     return {
-        localStorageAvailable: localStorageAvailable
+        debounce: _debounce,
+        getContentProperty: _getContentProperty,
+        isBreakpointActive: _isBreakpointActive
     };
 
 })();
