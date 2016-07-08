ac62d6b192adabcc53d878d941560be0bc2158ae
Niels <niels@statik.be>
2016-07-08 11:25:25 +0200

    [to test / exclude] Split up map component

---
+++ js/components/map.js
@@ -0,0 +1,127 @@
+window.app = window.app || {};
+
+app.map = (function () {
+
+    var mapElement = document.getElementById('map-canvas');
+
+    var mapOptions = {
+        zoom: 16,
+        scrollwheel: false,
+        draggable: !Modernizr.touch,
+        styles: []
+    };
+
+    var _initialize = function () {
+        if (mapElement) {
+            if (typeof google !== 'undefined') {
+                this.load();
+            } else {
+                app.helpers.loadScript('https://maps.googleapis.com/maps/api/js?v=3.exp'
+                    + '&key=AIzaSyA90nWScunrXckdU368WTPvJ6bZWJLT81c&callback=app.map.load');
+            }
+        }
+    };
+
+    var _load = function () {
+
+        var latlng = mapElement.getAttribute('data-latlng');
+        var points = mapElement.getAttribute('data-points-obj');
+
+        if (latlng) {
+            this.showPoint(latlng);
+        } else if (points) {
+            this.showPoints(window[points]);
+        }
+
+    };
+
+    var _renderPoint = function (lat, lng) {
+
+        var myLatLng = new google.maps.LatLng(lat, lng);
+
+        var map = new google.maps.Map(mapElement, app.helpers.extend(mapOptions, { center: myLatLng });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map
+        });
+
+        google.maps.event.addDomListener(window, 'resize', app.helpers.debounce(function () {
+            map.setCenter(myLatLng);
+        }));
+
+    };
+
+    var _renderPoints = function (points) {
+
+        var markers = [];
+        var map = new google.maps.Map(mapElement, mapOptions);
+        var bounds = new google.maps.LatLngBounds();
+        var infoWindow = new google.maps.infoWindow({
+            content: 'Loading…'
+        });
+
+        var pointCount = points.length;
+
+        var point;
+        var latLng;
+        var marker;
+
+        for (var i = 0; i < pointCount; i++) {
+            point = points[i];
+            latLng = new google.maps.LatLng(point.lat, point.lng);
+
+            marker = new google.maps.Marker({
+                position: latLng,
+                map: map,
+                lat: tmpMarker.lat,
+                lng: tmpMarker.lng,
+            });
+
+            marker.id = i;
+            markers.push(marker);
+            bounds.extend(latLng);
+
+            google.maps.event.addListener(marker, 'click', function () {
+
+                infoWindow.setContent([
+                    '<div class="excerpt excerpt--infowindow">',
+                        '<button class="excerpt__close"></button>',
+                        '<div class="excerpt__header">',
+                            '<h3 class="excerpt__title">' + this.title + '</h3>',
+                        '</div>',
+                        '<div class="excerpt__main">',
+                            '<p>' + this.teaser + '</p>',
+                        '</div>',
+                    '</div>'
+                ].join('\n'));
+
+                infoWindow.open(map, this);
+
+            });
+
+        }
+
+        if (bounds.getNorthEast().equals(bounds.getSouthWest())) {
+            bounds.extend(new google.maps.LatLng(bounds.getNorthEast().lat() + 0.01, bounds.getNorthEast().lng() + 0.01));
+            bounds.extend(new google.maps.LatLng(bounds.getSouthWest().lat() - 0.01, bounds.getSouthWest().lng() - 0.01));
+        }
+
+        map.fitBounds(bounds);
+        map.setZoom(map.getZoom() - 1);
+
+        google.maps.event.addDomListener(window, 'resize', app.helpers.debounce(function () {
+            map.fitBounds(bounds);
+        }));
+
+    };
+
+    return {
+        init: _initialize,
+        load: _load,
+        showPoint: _showPoint,
+        showPoints: _showPoints
+    };
+
+
+})();
