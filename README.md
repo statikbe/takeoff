942ddd55c6dd98467fefd6cc96c0001d37e86941
Niels <niels@statik.be>
2017-06-13 09:18:43 +0200

    Implement icon task

--- README.md
+++ README.md
@@ -3,13 +3,11 @@ takeoff
 
 A starter kit for an HTML project with sass. You can put this in any folder you like, we normally put it inside **[root]/takeoff**
 
-After you've copied the files into the correct folder, get the needed dependencies with `$ npm install`. After all the needed packages are downloaded, you can run grunt with
+After you've copied the files into the correct folder, get the needed dependencies with `$ yarn install`. After all the needed packages are downloaded, you can run Gulp with
 
 ```
-$ grunt
+$ gulp
 ```
-
-Optionally you can specify a target with `--target=<target>`, the default target is **../application/public**. It will first build the whole project, en after that watch the takeoff folder for changes, and build when needed.
 
 Needed before you can use our takeoff
 
@@ -18,16 +16,49 @@ Needed before you can use our takeoff
 https://docs.npmjs.com/getting-started/installing-node
 ```
 
-## Xcode
-App Store
+## Gulp
 
-## Grunt
+https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md
+
+### Tasks
+
+#### CSS
+
 ```
-http://gruntjs.com/installing-grunt
+$ gulp css
 ```
 
-## Grunt packages dependencies
+1. Compiles Sass to CSS.
+2. Adds vendor prefixes where needed.
+3. Minifies into `<target>/css/main.min.css`.
+
+#### Javascript
+
 ```
-$ brew install ttfautohint fontforge --with-python
-$ brew install sassc
+$ gulp js
 ```
+
+1. Finds and reports javascript errors to the terminal.
+2. Concatenates source javascript files.
+3. Minifies into `<target>/js/main.min.js`.
+
+
+#### Images
+
+```
+$ gulp img
+```
+
+1. Minifies .svg, .png, .jpg, .gif and .ico files.
+2. Provides .png fallbacks for .svg images.
+3. Builds into `<target>/img/`.
+
+#### Fonts
+
+#### Build
+
+```
+$ gulp build
+```
+
+Runs all the above tasks.
---
+++ gulpfile.babel.js
@@ -0,0 +1,167 @@
+import gulp from 'gulp';
+import loadPlugins from 'gulp-load-plugins';
+import autoprefixer from 'autoprefixer';
+import pngquant from 'imagemin-pngquant';
+import del from 'del';
+import path from 'path';
+
+const plugins = loadPlugins();
+
+const pkg = require('./package.json');
+
+gulp.task('css', () => 
+    gulp.src('sass/main.scss')
+        .pipe(plugins.sass().on('error', handleError))
+        .pipe(plugins.postcss(postcssPlugins))
+        .pipe(plugins.cssnano())
+        .pipe(plugins.rename(minifiedRenameConfig))
+        .pipe(gulp.dest(targetDir('css')))
+        .pipe(plugins.notify('CSS task complete'))
+);
+
+gulp.task('jshint', () => 
+    gulp.src([
+        'js/main.js',
+        'js/components/*.js',
+        '!js/**/_*.js'  //  Exclude files that start with an underscore
+    ])
+        .pipe(plugins.jshint())
+        .pipe(plugins.jshint.reporter('jshint-stylish'))
+);
+
+gulp.task('js:main', () =>
+    gulp.src([
+        'js/libs/*.js',
+        'js/components/*.js',
+        'js/main.js',
+        '!js/**/_*.js'  //  Exclude files that start with an underscore
+    ])
+        .pipe(plugins.concat('main.js'))
+        .pipe(plugins.uglify())
+        .pipe(plugins.rename(minifiedRenameConfig))
+        .pipe(gulp.dest(targetDir('js')))
+        .pipe(plugins.notify('JS task complete'))
+);
+
+gulp.task('js:singles', () =>
+    gulp.src([
+        'js/singles/*.js',
+        '!js/**/_*.js'  //  Exclude files that start with an underscore
+    ])
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest(targetDir('js')))
+        .pipe(plugins.notify('JS task complete'))
+);
+
+gulp.task('js', gulp.series('jshint', gulp.parallel('js:main', 'js:singles')));
+
+gulp.task('img', () => 
+    gulp.src('img/**/*.{svg,png,jpg,gif,ico}')
+        .pipe(plugins.imagemin(imageminConfig))
+        .pipe(gulp.dest(targetDir('img')))
+        .pipe(svgFilter)
+        .pipe(plugins.svg2png())
+        .pipe(plugins.imagemin(imageminConfig))
+        .pipe(gulp.dest(targetDir('img/svg/fallback')))
+        .pipe(plugins.notify('IMG task complete'))
+);
+
+gulp.task('fonts', () => 
+    gulp.src('fonts/**/*.*')
+        .pipe(gulp.dest(targetDir('fonts')))
+);
+
+gulp.task('iconfont', () => 
+    gulp.src('icons/svg/**/*.svg')
+        .pipe(plugins.iconfont(iconfontConfig))
+        .on('glyphs', function (glyphs, options) {
+            gulp.src('icons/_template.scss')
+                .pipe(plugins.consolidate('lodash', {
+                    fontName: options.fontName,
+                    glyphs: glyphs
+                }))
+                .pipe(plugins.rename('_icons.scss'))
+                .pipe(gulp.dest('sass/core/'));
+        })
+        .pipe(gulp.dest(targetDir('fonts')))
+);
+
+gulp.task('build', gulp.series('iconfont', gulp.parallel('css', 'js', 'img', 'fonts')));
+
+gulp.task('watch', () => {
+
+    gulp.watch('sass/**/*.scss', gulp.task('css'));
+    gulp.watch('js/**/*.js', gulp.task('js'));
+    gulp.watch('img/**/*.{svg,png,jpg,gif,ico}', gulp.task('img'));
+    gulp.watch('fonts/**/*.*', gulp.task('fonts'));
+    gulp.watch('icons/**/*.*', gulp.task('iconfont'));
+});
+
+gulp.task('default', gulp.series('build', 'watch'));
+
+// CONFIGURATION
+
+const minifiedRenameConfig = {
+    suffix: '.min'
+};
+
+const postcssPlugins = [
+    autoprefixer({
+        browsers: ['last 2 versions']
+    })
+];
+
+const imageminConfig = {
+    progressive: true,
+    interlaced: true,
+    svgoPlugins: [
+        { removeViewBox: false },
+        { cleanupIDs: false },
+        { removeUselessStrokeAndFill: false }
+    ],
+    use: [ pngquant() ]
+};
+
+const timeStamp = Math.round(Date.now() / 1000);
+
+const iconfontName = 'icons';
+
+const iconfontConfig = {
+    // formats: ,
+    // autohint: ,
+    fontName: iconfontName,
+    // fontWeight: ,
+    // fontStyle: ,
+    // fixedWidth: ,
+    // centerHorizontally: ,
+    normalize: true,
+    // fontHeight: ,
+    // round: ,
+    // descent: ,
+    // metadata: ,
+    // log: ,
+    // startUnicode: ,
+    prependUnicode: true,
+    timeStamp: timeStamp
+};
+
+const svgFilter = plugins.filter('**/*.svg');
+
+//  HELPER FUNCTIONS
+
+//  Log and notify errors
+function handleError(error) {
+
+    plugins.util.log(`${plugins.util.colors.red.bold(`[✘] ${error.plugin}`)} error in ${plugins.util.colors.underline(error.relativePath)}:\n
+        ${error.messageOriginal} (line ${error.line}, column ${error.column})\n`);
+
+    return plugins.notify.onError({
+        title: `${error.plugin} error in ${plugins.util.colors.underline(error.relativePath)}`,
+        message: `${error.messageOriginal} (line ${error.line}, column ${error.column})`
+    })(error);
+}
+
+//  Helper function for gulp.dest() to make sure 'target' in package.json is used as target directory.
+function targetDir(dir) {
+    return path.join(pkg.config.target, dir);
+}
