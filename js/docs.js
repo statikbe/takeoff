951b8372df8c5babafd192b7538e80e38920a438
Wietse De Cnijf <wietse@statik.be>
2019-01-16 18:15:24 +0100

    update filter js and provided documentation

--- js/docs.js
+++ js/docs.js
@@ -1,10 +1,10 @@
 'use strict';
 
-import $ from 'jquery';
-
 import './components/common';
 import './components/form';
 import './components/flyout';
+import './components/filter';
+import './components/code-block';
 
 //  Icon font generation - do not remove
 import '../icons/iconfont.font';
